db347fa9d2132327e744727f189474e46b534f36
guillemdiaz <gdiazcabanas@gmail.com>
2026-04-30 19:07:25 +0200

    chore: initial project setup

---
+++ src/main/java/dev/guillemdiaz/herald/HeraldApplication.java
@@ -0,0 +1,13 @@
+package dev.guillemdiaz.herald;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeraldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeraldApplication.class, args);
+    }
+
+}
